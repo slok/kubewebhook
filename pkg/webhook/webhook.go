a34c4cfa1a7106e0209830418e05848788d64387
Xabier Larrakoetxea <slok69@gmail.com>
2018-07-07 12:10:00 +0200

    Add webhook interface
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

---
+++ pkg/webhook/webhook.go
@@ -0,0 +1,13 @@
+package webhook
+
+import (
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+)
+
+// Webhook knows how to handle the admission reviews, in other words Webhook is a dynamic
+// admission webhook for Kubernetes.
+type Webhook interface {
+	// Review will handle the admission reviewand return the AdmissionResponse with the result of the admission
+	// error, mutation...
+	Review(ar *admissionv1beta1.AdmissionReview) *admissionv1beta1.AdmissionResponse
+}
