cc03758b9c98713504d9610c3d0911e0e811244a
Xabier Larrakoetxea <me@slok.dev>
2020-03-07 08:52:49 +0100

    Add CRD generation for integration tests
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

---
+++ hack/scripts/integration-test-crd.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Add all groups space separated.
+GROUPS_VERSION="building:v1"
+
+# Only generate deepcopy (runtime object needs) and typed client.
+# Typed listers & informers not required for the moment. Used with generic
+# custom informer/listerwatchers.
+TARGETS="deepcopy,client"
+
+IMAGE=quay.io/slok/kube-code-generator
+DIR="$( cd "$( dirname "${0}" )" && pwd )"
+ROOT_DIR=${DIR}/../..
+PROJECT_PACKAGE=github.com/slok/kubewebhook
+CRD_PACKAGE=github.com/slok/kubewebhook/test/integration/crd/apis
+
+
+docker run -it --rm \
+        -v ${ROOT_DIR}:/go/src/${PROJECT_PACKAGE} \
+        -e PROJECT_PACKAGE="${PROJECT_PACKAGE}" \
+        -e CLIENT_GENERATOR_OUT="${PROJECT_PACKAGE}/test/integration/crd/client" \
+        -e APIS_ROOT="${CRD_PACKAGE}" \
+        -e GROUPS_VERSION="${GROUPS_VERSION}" \
+        -e GENERATION_TARGETS="${TARGETS}" \
+        ${IMAGE} 
+
+docker run -it --rm \
+	-v ${ROOT_DIR}:/src \
+        -e GO_PROJECT_ROOT=/src \
+	-e CRD_TYPES_PATH=/src/test/integration/crd/apis \
+	-e CRD_OUT_PATH=/src/test/integration/crd/manifests \
+	quay.io/slok/kube-code-generator update-crd.sh
---
+++ test/integration/crd/apis/building/v1/doc.go
@@ -0,0 +1,5 @@
+// +k8s:deepcopy-gen=package
+// +groupName=building.kubewebhook.slok.dev
+
+// Package v1 is the v1 version of the API.
+package v1
