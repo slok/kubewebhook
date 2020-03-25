53ae54d86478a86f49397a055beaad0e65bc8cef
xuxinkun <xuxinkun@gmail.com>
2020-03-25 14:13:45 +0800

    make AdmissionRequestKey public
    Signed-off-by: xuxinkun <xuxinkun@gmail.com>

--- pkg/webhook/context/context.go
+++ pkg/webhook/context/context.go
@@ -6,20 +6,22 @@ import (
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
 )
 
-type contextKey string
+type contextKey struct {
+	name string
+}
 
-var admissionRequestKey = contextKey("admissionRequest")
+var AdmissionRequestKey = &contextKey{"admissionRequest"}
 
 // SetAdmissionRequest will set a admission request on the context and return the new context that has
 // the admission request set.
 func SetAdmissionRequest(ctx context.Context, ar *admissionv1beta1.AdmissionRequest) context.Context {
-	return context.WithValue(ctx, admissionRequestKey, ar)
+	return context.WithValue(ctx, AdmissionRequestKey, ar)
 }
 
 // GetAdmissionRequest returns the admission request stored on the context. If there is no admission
 // request on the context it will return nil.
 func GetAdmissionRequest(ctx context.Context) *admissionv1beta1.AdmissionRequest {
-	val := ctx.Value(admissionRequestKey)
+	val := ctx.Value(AdmissionRequestKey)
 	if ar, ok := val.(*admissionv1beta1.AdmissionRequest); ok {
 		return ar
 	}
