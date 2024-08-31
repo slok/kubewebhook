767545d1e44d9abeb7653002901c1355dd47e8d2
Xabier Larrakoetxea <me@slok.dev>
2024-08-31 09:04:45 +0200

    Update kube-code-generator to v0.3.1
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

--- test/integration/gen-crd.sh
+++ test/integration/gen-crd.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-docker run -it --rm -v ${PWD}:/app ghcr.io/slok/kube-code-generator:v0.3.0 \
+docker run -it --rm -v ${PWD}:/app ghcr.io/slok/kube-code-generator:v0.3.1 \
         --apis-in ./test/integration/crd/apis \
         --go-gen-out ./test/integration/crd/client \
         --crd-gen-out ./test/integration/crd/manifests
